266001b5a23ed5f76d333dbd52136eb7ee502b3e
canhho144 <canhhtde181016@fpt.edu.vn>
2026-09-15 14:23:06 +0700

    feat: Todo0.7#  DELETE: EmployeeDAO.delete(Long id)

--- Chapter-JPA-Ex1/src/main/java/fu/se20b01/EmployeeDAO.java
+++ Chapter-JPA-Ex1/src/main/java/fu/se20b01/EmployeeDAO.java
@@ -108,4 +108,30 @@ public class EmployeeDAO {
             em.close();
         }
     }
+
+    public void delete(Long id) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            Employee e = em.find(Employee.class, id);
+
+            if (e != null) {
+                em.remove(e);
+            }
+
+            em.getTransaction().commit();
+
+        } catch (Exception ex) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw ex;
+
+        } finally {
+            em.close();
+        }
+    }
 }
+
