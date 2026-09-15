448b2bd44aa3892d110ef4c3a2b9c30c47653cf9
canhho144 <canhhtde181016@fpt.edu.vn>
2026-09-15 14:11:50 +0700

    feat:TODO 0.4# findById and findAll

--- Chapter-JPA-Ex1/src/main/java/fu/se20b01/EmployeeDAO.java
+++ Chapter-JPA-Ex1/src/main/java/fu/se20b01/EmployeeDAO.java
@@ -4,37 +4,48 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+
 public class EmployeeDAO {
 
-    private final EntityManagerFactory emf;
-
-    public EmployeeDAO() {
-        emf = Persistence.createEntityManagerFactory("defaultPU");
-    }
+    private final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("defaultPU");
 
     public void save(Employee e) {
-
         EntityManager em = emf.createEntityManager();
 
         try {
             em.getTransaction().begin();
             em.persist(e);
             em.getTransaction().commit();
-
         } catch (Exception ex) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
             }
-
             throw ex;
-
         } finally {
-
             em.close();
         }
     }
+    public Employee findById(Long id) {
+        EntityManager em = emf.createEntityManager();
 
-    public void close() {
-        emf.close();
+        try {
+            return em.find(Employee.class, id);
+        } finally {
+            em.close();
+        }
+    }
+    public List<Employee> findAll() {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.createQuery(
+                    "SELECT e FROM Employee e",
+                    Employee.class
+            ).getResultList();
+        } finally {
+            em.close();
+        }
     }
 }
