63420df69f468d17d3af19f1f5a2a4b3c8f293aa
canhho144 <canhhtde181016@fpt.edu.vn>
2026-09-18 12:00:59 +0700

    feat(TODO2.7): Demo trong Main

--- slot3/Chapter1_JPA_MappingOneToMany/src/main/java/fu/Main.java
+++ slot3/Chapter1_JPA_MappingOneToMany/src/main/java/fu/Main.java
@@ -4,6 +4,7 @@ import fu.dao.DepartmentDAO;
 import fu.pojo.Department;
 import fu.pojo.Employee;
 import fu.pojo.Gender;
+import fu.util.JPAUtil;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,28 +13,53 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Department dept = new Department("IT", "Ha Noi");
-
-        Employee emp = new Employee(
-                "Test",
-                "test@company.com",
-                Gender.OTHER,
-                true,
-                new BigDecimal("1000"),
-                LocalDate.now()
-        );
-
-        dept.addEmployee(emp);
-
         DepartmentDAO departmentDAO = new DepartmentDAO();
 
-        departmentDAO.save(dept);
+        Department it = new Department("Marketing", "Ha Noi");
 
-        Department result =
-                departmentDAO.findByIdWithEmployees(dept.getId());
+        Employee e1 = new Employee(
+                "Nguyen Van A",
+                "aa.nguyen@company.com",
+                Gender.MALE,
+                true,
+                new BigDecimal("15000000"),
+                LocalDate.of(2022, 1, 10)
+        );
 
-        System.out.println("Department: " + result.getName());
-        System.out.println("Number of employees: "
-                + result.getEmployees().size());
+        Employee e2 = new Employee(
+                "Tran Thi B",
+                "bb.tran@company.com",
+                Gender.FEMALE,
+                true,
+                new BigDecimal("18000000"),
+                LocalDate.of(2021, 6, 1)
+        );
+
+        Employee e3 = new Employee(
+                "Le Van C",
+                "cc.le@company.com",
+                Gender.OTHER,
+                true,
+                new BigDecimal("12000000"),
+                LocalDate.of(2023, 3, 15)
+        );
+
+        it.addEmployee(e1);
+        it.addEmployee(e2);
+        it.addEmployee(e3);
+
+        departmentDAO.save(it);
+
+        System.out.println("Da luu Department, id = " + it.getId());
+
+        Department found = departmentDAO.findByIdWithEmployees(it.getId());
+
+        System.out.println("Phong ban: " + found.getName());
+
+        for (Employee e : found.getEmployees()) {
+            System.out.println(" - " + e);
+        }
+
+        JPAUtil.close();
     }
 }
