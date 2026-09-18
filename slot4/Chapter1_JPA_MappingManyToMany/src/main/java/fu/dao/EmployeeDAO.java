bed5588994968c7fc116f3647ecf9737a5d6a34a
canhho144 <canhhtde181016@fpt.edu.vn>
2026-09-18 14:25:08 +0700

    feat(TODO5.6): Write method assignEmployeeToProject

--- slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fu/dao/EmployeeDAO.java
+++ slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fu/dao/EmployeeDAO.java
@@ -1,6 +1,7 @@
 package fu.dao;
 
 import fu.pojo.Employee;
+import fu.pojo.Project;
 import fu.util.JPAUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
@@ -87,6 +88,42 @@ public class EmployeeDAO {
             }
 
             transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void assignEmployeeToProject(Long employeeId, Long projectId) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+
+            Employee employee = em.find(Employee.class, employeeId);
+            Project project = em.find(Project.class, projectId);
+
+            if (employee == null) {
+                throw new IllegalArgumentException(
+                        "Employee not found: " + employeeId
+                );
+            }
+
+            if (project == null) {
+                throw new IllegalArgumentException(
+                        "Project not found: " + projectId
+                );
+            }
+
+            employee.assignToProject(project);
+
+            transaction.commit();
+
         } catch (Exception e) {
             if (transaction.isActive()) {
                 transaction.rollback();
