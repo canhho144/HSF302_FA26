fcd566c825879ca13a0307b0414366dfa5c3bd15
canhho144 <canhhtde181016@fpt.edu.vn>
2026-09-18 13:51:29 +0700

    feat(TODO5.2): Owning Side

--- slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fu/pojo/Employee.java
+++ slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fu/pojo/Employee.java
@@ -3,6 +3,8 @@ package fu.pojo;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "employees")
@@ -29,6 +31,14 @@ public class Employee {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id", nullable = false)
     private Department department;
+
+    @ManyToMany
+    @JoinTable(
+            name = "employee_project",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private Set<Project> projects = new HashSet<>();
 
     public Employee() {
     }
